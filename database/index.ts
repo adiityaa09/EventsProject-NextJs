aaae23ccf9b61aebb35b1dffad87a18b3e226c44
adiityaa09 <aditya.25bcs10242@sst.scaler.com>
2026-07-11 04:15:08 +0530

    make a change for the env only if we need to use it

---
+++ database/index.ts
@@ -0,0 +1,8 @@
+//database models export
+export {default as Event} from './event.model';
+export {default as Booking} from './booking.model'
+
+
+//typescript interface export
+export type {IEvent} from './event.model';
+export type {IBooking} from './booking.model';
